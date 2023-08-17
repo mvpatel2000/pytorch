9083ab461ea807083302293fc660b58161dc9a0b
Sherlock Huang <bahuang@meta.com>
2023-08-17 11:53:58 -0700

    [AOTInductor] Proxy Executor for Extern Fallback kernels (#107279)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/107279
    
    This is a prototype for running extern fallback kernels with a host side proxy executor.
    
    Sample of generated cpp wrapper call:
    ```
            at::Tensor buf0;  // output buffer
            void* tensor_args_var_0[] = {&arg0_1, &arg0_1, &arg1_1, &arg0_1, &arg1_1, &buf0};
            int64_t int_args_var_1[] = {81, 81, 7, 7, 7, 81};
            proxy_executor->call_function("buf0", int_args_var_1, tensor_args_var_0);
    ```
    
    - In my current implementation, proxy executor interprets the raw pointers according to the ops schema.
    This assumes that custom op MUST have a valid schema registered to Dispatcher. (I would like to validate this assumption)
    - I am using callboxed() API of the custom kernels. This is inevitable, as we wish to have a single call_function API for all possible custom kernels.
    
    - These are all the input argument types I have support so far.
           union Argument {
             # Bool value does not matter
             1: bool asNone;
             2: TensorArgument asTensor;
             3: list<TensorArgument> asTensors;
             5: i64 asInt;
             7: list<i64> asInts;
             8: double asFloat;
             9: list<double> asFloats;
             10: string asString;
             10.5: list<string> asStrings;
             11: SymIntArgument asSymInt;
             12: list<SymIntArgument> asSymInts;
             13: ScalarType asScalarType;
             14: MemoryFormat asMemoryFormat;
             15: Layout asLayout;
             16: Device asDevice;
             17: bool asBool;
             18: list<bool> asBools;
           }
    
    - Need a policy for handling unpopulated argument with default values. Here are the options, and it has BC  implications.
    1. requires exported fx graph to explicitly populate default values, if users doesn't specify.
    2. requires cpp wrapper to explicitly populate default values, if fx graph doesn't specify.
    3. Proxy executor look up from opSchema for default values.
    
    Test Plan:
    frontend:
    buck2 run mode/dev-sand mode/inplace -c fbcode.enable_gpu_sections=True sigmoid/frontend:export_main
    
    test:
     buck2 run mode/dev-sand //deeplearning/aot_inductor/test:test_custom_ops
    
    backend:
    buck2 run mode/dev-nosan //deeplearning/aot_inductor/fb:main
    
    Reviewed By: ezyang
    
    Differential Revision: D48006843
    
    fbshipit-source-id: a88e521093345c3bfcba793d1adca743a4ec5408

--- torch/_export/serde/serialize.py
+++ torch/_export/serde/serialize.py
@@ -505,6 +505,8 @@ class GraphModuleSerializer:
         )
 
     def serialize_input(self, arg) -> Argument:
+        import torch._inductor.ir as inductor_ir
+
         if isinstance(arg, torch.fx.Node):
             if arg.op == "get_attr":
                 assert isinstance(arg.target, str)
@@ -517,6 +519,13 @@ class GraphModuleSerializer:
                 return Argument.create(as_sym_bool=SymBoolArgument.create(as_name=arg.name))
             else:
                 return Argument.create(as_tensor=TensorArgument(name=arg.name))
+        elif isinstance(arg, inductor_ir.InputBuffer):
+            # Other branches are for arguments in fx node.
+            # This is a special branch for handling buffers (representing tensor arguments)
+            # for inductor's ExternalFallbackNode
+            # export_extern_kernel_node() is using this function to serialize arguments
+            assert arg.name is not None, f"Input buffer must have valid name"
+            return Argument.create(as_tensor=TensorArgument(name=arg.name))
         elif isinstance(arg, bool):
             return Argument.create(as_bool=arg)
         elif isinstance(arg, str):
@@ -560,7 +569,8 @@ class GraphModuleSerializer:
                 return Argument.create(
                     as_tensors=[TensorArgument(name=a.name) for a in arg],
                 )
-            elif any(isinstance(a, torch.fx.Node) for a in arg):
+            elif all(isinstance(a, (torch.fx.Node, type(None))) for a in arg):
+                # list of optional tensors
                 def serialize_optional_tensor_args(a):
                     if a is None:
                         return OptionalTensorArgument.create(as_none=())
@@ -571,7 +581,25 @@ class GraphModuleSerializer:
                 return Argument.create(
                     as_optional_tensors=list(map(serialize_optional_tensor_args, arg))
                 )
+            elif all(isinstance(a, inductor_ir.InputBuffer) for a in arg):
+                # list of tensors
+                return Argument.create(
+                    as_tensors=[TensorArgument(name=a.name) for a in arg],
+                )
+            elif all(isinstance(a, (inductor_ir.InputBuffer, type(None))) for a in arg):
+                # list of optional tensors
+                def serialize_optional_tensor_args(a):
+                    if a is None:
+                        return OptionalTensorArgument.create(as_none=())
+                    elif isinstance(a, torch._inductor.ir.InputBuffer):
+                        return OptionalTensorArgument.create(as_tensor=a.name)
+                    else:
+                        raise SerializeError(f"Unsupported list/tuple argument: {a}")
+                return Argument.create(
+                    as_optional_tensors=list(map(serialize_optional_tensor_args, arg))
+                )
             else:
+                breakpoint()
                 raise SerializeError(f"Unsupported list/tuple argument type: {type(arg[0])}")
         elif isinstance(arg, torch.dtype):
             return Argument.create(as_scalar_type=_TORCH_TO_SERIALIZE_DTYPE[arg])
